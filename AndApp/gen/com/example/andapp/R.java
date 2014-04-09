185fa188eccf38b0947267bef375acfc36b32d4a
FLEDevgit <francois.a.lejeune@gmail.com>
2014-04-09 13:12:33 +0200

    Keumit

--- AndApp/gen/com/example/andapp/R.java
+++ AndApp/gen/com/example/andapp/R.java
@@ -39,7 +39,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int hello_world_widget=0x7f060002;
+        public static final int widget_titre=0x7f060002;
     }
     public static final class style {
         /** 
