b654ee7def70dc5b3984bc9887ed426201de59de
mathieu <mathieu@mathieu-G53SX>
2014-04-15 23:09:49 +0200

    Début de la dynamisation du widget

--- AndApp/src/com/example/andapp/ConfigActivity.java
+++ AndApp/src/com/example/andapp/ConfigActivity.java
@@ -1,43 +1,52 @@
 package com.example.andapp;
 
-import com.example.andapp.service.CBDDUtils;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.format.DateUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class ConfigActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
 		setContentView(R.layout.activity_config);
 
 		 // cancel button
         Button btnCancel = (Button) findViewById(R.id.buttonCancel);
         btnCancel.setOnClickListener(new OnClickListener() {
-
             @Override
             public void onClick(View view) {
-            	DatePicker dp = (DatePicker) findViewById(R.id.pickerTargetDate);
-            	Log.w("", dp.getYear()+"-" +dp.getMonth()+"-"+dp.getDayOfMonth());
-            	Log.w("", "C'est dans : "+CBDDUtils.getSleepsNrToYearMonthDay(dp.getYear(),dp.getMonth(), dp.getDayOfMonth())+" dodos");
                 finish();
             }
         });
 		
-		
-//		if (savedInstanceState == null) {
-//			getSupportFragmentManager().beginTransaction()
-//					.add(R.id.scrollableContentHolder, new PlaceholderFragment()).commit();
-//		}
+        Button btnOk = (Button) findViewById(R.id.buttonOk);
+        btnOk.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            	DatePicker dp = (DatePicker) findViewById(R.id.pickerTargetDate);
+            	EditText eventName = (EditText) findViewById(R.id.eventName);
+            	Intent intent = new Intent(WidgetActivity.ACTION_CONFIGURATION_CHANGED);
+            	intent.putExtra("eventYear", dp.getYear());
+            	intent.putExtra("eventMonth", dp.getMonth());
+            	intent.putExtra("eventDay", dp.getDayOfMonth());
+            	intent.putExtra("eventName", eventName.getText().toString());
+            	
+                getApplicationContext().sendBroadcast(intent);
+                finish();
+            }
+        });
+        
 	}
 
 	@Override
--- AndApp/src/com/example/andapp/WidgetActivity.java
+++ AndApp/src/com/example/andapp/WidgetActivity.java
@@ -1,31 +1,64 @@
 package com.example.andapp;
 
+import com.example.andapp.service.CBDDUtils;
+
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
 public class WidgetActivity extends AppWidgetProvider {
+	public static final String ACTION_CONFIGURATION_CHANGED = "com.example.andapp.CONFIGURATION_CHANGED";
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 	    RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_cbdd);
 	    Intent configIntent = new Intent(context, Activity.class);
-
-	  
 	    
-	    // configure the click behavior of the widget
         Intent intent = new Intent(context, ConfigActivity.class);
-        // this causes each widget to have a unique PendingIntent
-//        Uri data = Uri.withAppendedPath(Uri.parse("peregin://widget/id/"), String.valueOf(widgetId));
-//        intent.setData(data);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 	    
 	    remoteViews.setOnClickPendingIntent(R.id.widget_container, pendingIntent);
 	    appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
 	}   
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	    super.onReceive(context, intent);
+	    if (intent.getAction().equals(ACTION_CONFIGURATION_CHANGED)) {
+	        // handle intent here
+	        
+	        update(context,intent);
+	    }
+	    super.onReceive(context, intent);
+	}
+	
+	
+	 private void update(Context context, Intent intent) {
+	        ComponentName widget = new ComponentName(context, this.getClass());
+	        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+	        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(widget);
+
+	        for (int widgetId : appWidgetIds) {
+	            RemoteViews views = updateRemoteViews(context, widgetId,intent);
+	            appWidgetManager.updateAppWidget(widgetId, views);
+	        }
+	    }
+
+	    private RemoteViews updateRemoteViews(Context context, int widgetId, Intent intent) {
+	    	RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.activity_cbdd);
+	        String eventName = intent.getStringExtra("eventName");
+	        int year = intent.getIntExtra("eventYear",0);
+	        int month = intent.getIntExtra("eventMonth",0);
+	        int dayOfMonth = intent.getIntExtra("eventDay",0);
+	        views.setTextViewText(R.id.textCount, CBDDUtils.getSleepsNrToYearMonthDay(year, month, dayOfMonth) + "");
+
+
+
+	        return views;
+	    }
 }
